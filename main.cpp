5b5086663f1fbab0d87d74fbcb27c535639308b5
Tsasng-Tsz-Ho <127906711+Tsasng-Tsz-Ho@users.noreply.github.com>
2023-04-28 12:14:02 +0800

    feat: main.cpp prints the title and commands

--- main.cpp
+++ main.cpp
@@ -1,2 +1,28 @@
 //The file that contain the main function
 #include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main(){
+	string command;
+	cout<<setw(45);
+	cout<<"Shoot-Out Game"<<endl;
+	cout<<"Enter New to start a new game, Load to load saved game, and Exit to terminate"<<endl;
+	cin>>command;
+	if(command=="New"){
+		//initiate a new game status;
+	}
+	else if(command=="Load"){
+		//load the saved game status;
+	}
+	else if(command=="Exit"){
+		return 0;
+	}
+	else{
+		cout<<"Invalid command, please restart"<<endl;
+		return 0;
+	}
+	//implementation of the game
+	
+}

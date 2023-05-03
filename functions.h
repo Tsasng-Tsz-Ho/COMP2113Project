5216690792f8e87435b5b16b0c5735ef5fe97533
Ernestyltsang <125571142+Ernestyltsang@users.noreply.github.com>
2023-05-03 23:37:44 +0800

    Update functions.h

--- functions.h
+++ functions.h
@@ -15,5 +15,6 @@ void SwapPlayers(vector<Player> v1,Player p1,Player p2);
 void PrintAll(vector<Player> v2);
 int Score(Player shooter,Player gk);
 void GamePlay(vector<Player> &user_team,vector<Player> &user_squad, vector<Player> opponent, int &level);
+string CompDirection(int d);
 
 #endif

8b02dd0ad13154941de0328a3d59bb7b1708a1b5
Tsasng-Tsz-Ho <127906711+Tsasng-Tsz-Ho@users.noreply.github.com>
2023-05-03 13:01:59 +0000

    Added PrintAll.cpp It includes function PrintAll which prints out the players

---
+++ PrintAll.cpp
@@ -0,0 +1,11 @@
+//It prints out all the players in the team
+#include <iostream>
+#include "functions.h"
+using namespace std;
+
+void PrintAll(vector<Player> team){
+    for(int i=0,i<team.size(),i++){
+        if(team[i].type=="GK"){cout<<"Goal Keeper: ";}else{cout<<"Offensive: ";}
+        cout<<team[i].name<<" "<<team[i].power<<endl;
+    }
+}

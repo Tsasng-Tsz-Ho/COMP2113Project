49db98ceff16658a2fcdc7fc004fb9273685d9fb
Ernestyltsang <125571142+Ernestyltsang@users.noreply.github.com>
2023-05-03 23:34:05 +0800

    Update PrintAll.cpp

--- PrintAll.cpp
+++ PrintAll.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void PrintAll(vector<Player> team){
   for(int i=0,i<team.size(),i++){
-    if(team[i].type=="GK"){cout<<"Goal Keeper: ";}else{cout<<"Offensive: ";}
+    if(team[i].type=="GK"){cout<<"Goalkeeper: ";}else{cout<<"Offensive: ";}
     cout<<team[i].name<<" "<<team[i].power<<endl;
   }
 }

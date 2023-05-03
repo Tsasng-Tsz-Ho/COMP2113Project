186e9ad2525eda27add073b7b03ccaf9af75faea
Tsasng-Tsz-Ho <127906711+Tsasng-Tsz-Ho@users.noreply.github.com>
2023-05-03 23:03:10 +0800

    Add comment && Simplify Score.cpp
    Simplified 2 if else statements to if statements as return ensures no further execution already and no else is needed

--- Score.cpp
+++ Score.cpp
@@ -1,3 +1,4 @@
+//Determine whether the shooter score and return 1 if score and 0 if not score
 #include "functions.h"
 #include <iostream>
 #include <string>
@@ -5,24 +6,21 @@
 #include <vector>
 using namespace std;
 
-int Score(Player shooter,Player gk,string direction, string ran_direction){
+int Score(Player shooter,Player gk,string shooter_direction, string gk_direction){
   cout<<setw(45);
   if (OnTarget(shooter)==false){
     cout<<"He shot the ball straight to the sky! The pressure is surely getting to him!"<<endl;
     return 0;
+  }
+  if (shooter_direction!=gk_direction){
+    cout<<"He sends the keeper the wrong way! What a pen!"<<endl;
+    return 1;
+  }
+  if (ScorePastKeeper(shooter,gk)){
+    cout<<"The keeper had his hands on the ball, but the ball was just striked with so much force!"<<endl;
+    return 1;
   } else{
-    if (direction!=ran_direction){
-      cout<<"He sends the keeper the wrong way! What a pen!"<<endl;
-      return 1;
-    } else{
-      if (ScorePastKeeper(shooter,gk)){
-          cout<<"The keeper had his hands on the ball, but the ball was just striked with so much force!"<<endl;
-          return 1;
-      } else{
-          cout<<"What a read by the goalkeeper! A magnificent save!"<<endl;
-          return 0;
-      }
-   }
- }
+    cout<<"What a read by the goalkeeper! A magnificent save!"<<endl;
+    return 0;
+  }
 }
-    

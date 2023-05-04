78cda8f2836193c55febd9b47b2fd9405681e6f5
Tsasng-Tsz-Ho <127906711+Tsasng-Tsz-Ho@users.noreply.github.com>
2023-05-05 01:42:40 +0800

    Create CreateTeam.cpp

---
+++ CreateTeam.cpp
@@ -0,0 +1,29 @@
+#include "functions.h"
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<Player> CreateTeam(ifstream fin){
+  //It makes a team
+  //It takes a input file stream as arguement
+  //and out put a vector of players as a team
+  string input;
+  int count;
+  Player temp;
+  vector<Player> result;
+  while(fin>>input){
+    if(count%3==0){
+      temp.type=input;
+      count++
+    }else if(count%3==1){
+      temp.name=input;
+      count++;
+    }else{
+      temp.power=stoi(input);
+      result.push_back(temp);
+      count++;
+    }
+  }
+  return result;
+}

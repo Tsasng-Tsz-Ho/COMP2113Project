38bc1df5a66d17992f37cdef8e35fe5f6f19d456
Ernestyltsang <125571142+Ernestyltsang@users.noreply.github.com>
2023-05-04 17:28:24 +0800

    Create Search.cpp

---
+++ Search.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "functions.h"
+using namespace std;
+
+int Search(vector<Player user_team,string name){
+  for (int i=0;i<user_name.size();i++){
+    if (user_team[i].name==name){
+      return i;
+    }
+  }
+  return -1;
+}

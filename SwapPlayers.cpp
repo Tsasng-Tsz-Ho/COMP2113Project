9151f4b95a0cc298bcde03e26999e22b34128fb9
Ernestyltsang <125571142+Ernestyltsang@users.noreply.github.com>
2023-05-04 17:09:30 +0800

    Update SwapPlayers.cpp

--- SwapPlayers.cpp
+++ SwapPlayers.cpp
@@ -5,18 +5,17 @@
 #include <iomanip>
 using namespcae std;
 
-vector<Player> SwapPlayers(vector<Player> v, Player p1, Player p2){
+void SwapPlayers(vector<Player> v, Player p1, Player p2){
   cout<< setw(45);
   if (p1.type!=p2.type){
     cout<< "Substitute the player with another player of the same position."<< endl;
-    return v;
-  } else{
-      for (int i=0;i<6;i++){
+    return;
+  } 
+  for (int i=0;i<6;i++){
         if (v[i]==p2){
           v[i]=p1;
         }
-      }
-    return v;
+    return;
   }
 }
   

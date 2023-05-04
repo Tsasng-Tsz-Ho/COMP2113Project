c7bcf3cda7b63103567bbe9f6f52662f6b8196d4
Ernestyltsang <125571142+Ernestyltsang@users.noreply.github.com>
2023-05-04 23:38:01 +0800

    Update functions.h

--- functions.h
+++ functions.h
@@ -19,7 +19,6 @@ vector<Player> CreateTeam(ifstream x);
 bool OnTarget(Player x);
 bool ScorePastKeeper(Player y,Player z);
 void Booster(vector<Player> &user_team, string name,int k);
-//void SwapPlayers(vector<Player> v1,Player p1,Player p2);
 void PrintAll(vector<Player> v2);
 int Score(Player shooter,Player gk,string shooter_direction,string gk_direction);
 int Search(vector<Player> user_team,string name);

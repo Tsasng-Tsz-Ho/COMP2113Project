61795ab502e4a7539d8f13e8bcf1d93657070de8
Tsasng-Tsz-Ho <127906711+Tsasng-Tsz-Ho@users.noreply.github.com>
2023-05-05 14:46:42 +0800

    Update Booster.cpp
    delete duplicated cout

--- Booster.cpp
+++ Booster.cpp
@@ -9,7 +9,6 @@ void Booster(vector<Player> &user_team, string name,int k){
   //Arg 2: the name of the Player wanted to boost as string
   //Arg 3: the increase in power of the Player
   //return value: none, the increase would be reflected in the vector passed by reference
-  cout<<"The maximum power of a player is 90."<<endl;
   while (Search(user_team,name)==-1){
     cout<<"There is no such player in your team."<<endl;
     cout<<"Enter the name of the player you wish to boost."<<endl;

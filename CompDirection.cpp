097673feb6201904d404651a4ad5fce6ca0f811a
Ernestyltsang <125571142+Ernestyltsang@users.noreply.github.com>
2023-05-05 20:50:37 +0800

    Update CompDirection.cpp

--- CompDirection.cpp
+++ CompDirection.cpp
@@ -1,15 +1,14 @@
 #include "functions.h"
 #include <iostream>
 #include <string>
+#include <cstdlib>
 using namespace std;
 
-string CompDirection(int num){
+string CompDirection(){
   //It receives integer and returns direction in string, it returns L for 0, M for 1, and R for 2
-  if (num==0){
-    return "L";
-  } else if (num==1){
-    return "M";
-  } else{
-    return "R";
-  }
+  int t1 = rand()%60+1;
+  if (t1 <= 20) {return "L";}
+  else if (t1>20 && t1<=40) {return "M";}
+  else {return "R";}
 }
+  

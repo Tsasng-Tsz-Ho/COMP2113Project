c0b678d014d5a09f24125566f87d47d7a55ef259
Tsasng-Tsz-Ho <127906711+Tsasng-Tsz-Ho@users.noreply.github.com>
2023-05-05 14:51:21 +0800

    Update OnTarget.cpp
    Add comment and change name of variable to improve readability

--- OnTarget.cpp
+++ OnTarget.cpp
@@ -1,12 +1,12 @@
-//The function returns a boolean value based on comparison between a random value and the Player's power. It determines whether the shooter shoots on target
 #include "functions.h"
 #include <iostream>
 #include <string>
 using namespace std;
 
-bool OnTarget(Player x){
+bool OnTarget(Player shooter){
+  //The function returns a boolean value based on comparison between a random value and the Player's power. It determines whether the shooter shoots on target
   int check = rand()%99+1;
-  if (x.power >= check){
+  if (shooter.power >= check){
     return true;
   }
   return false;

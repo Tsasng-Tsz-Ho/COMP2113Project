c66237c0cb4d3ea4d2430d360ce642797c40c868
Tsasng-Tsz-Ho <127906711+Tsasng-Tsz-Ho@users.noreply.github.com>
2023-05-05 14:35:26 +0800

    Change Comment CompDirection.cpp

--- CompDirection.cpp
+++ CompDirection.cpp
@@ -1,10 +1,10 @@
-//It receives integer and returns direction in string, it returns L for 0, M for 1, and R for2
 #include "functions.h"
 #include <iostream>
 #include <string>
 using namespace std;
 
 string CompDirection(int num){
+  //It receives integer and returns direction in string, it returns L for 0, M for 1, and R for 2
   if (num==0){
     return "L";
   } else if (num==1){

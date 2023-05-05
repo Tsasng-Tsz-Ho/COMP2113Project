90e9d931ab87356d023b9f99a03797e0bfdfe5cb
Tsasng-Tsz-Ho <127906711+Tsasng-Tsz-Ho@users.noreply.github.com>
2023-05-05 15:22:07 +0800

    Comment ScorePastKeeper.cpp
    Added comment and changed name of argument

--- ScorePastKeeper.cpp
+++ ScorePastKeeper.cpp
@@ -1,12 +1,15 @@
-//It returns whether the shooter score when the goal keeper correctly guessed direction
 #include "functions.h"
 #include <cstdlib>
 #include <iostream>
 #include <string>
 using namespace std;
 
-bool ScorePastKeeper(Player y,Player z){
-  int spread = y.power-z.power+20; 
+bool ScorePastKeeper(Player shooter,Player gk){
+  //It returns whether the shooter score when the goal keeper correctly guessed direction
+  //Arg 1: the player eho shoot
+  //Arg 2: the player who is the goalkeeper
+  //return value: true if score, false if not score
+  int spread = shooter.power-gk.power+20; 
   int check = rand()%45;
   if (spread >= check){
     return true;

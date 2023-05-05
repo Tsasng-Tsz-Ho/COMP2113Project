2561eb811d89d5e3f1cae3e0d0381f09edd13b6a
Ernestyltsang <125571142+Ernestyltsang@users.noreply.github.com>
2023-05-05 21:50:39 +0800

    Update CompDirection.cpp

--- CompDirection.cpp
+++ CompDirection.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 string CompDirection(){
-  //It receives integer and returns direction in string, it returns L for 0, M for 1, and R for 2
+  //It generates a random integer to decide which side the computer chooses (0 to 20 chooses "L", 21 to 40 chooses "M", 41 to 60 chooses "R")
   int t1 = rand()%60+1;
   if (t1 <= 20) {return "L";}
   else if (t1>20 && t1<=40) {return "M";}

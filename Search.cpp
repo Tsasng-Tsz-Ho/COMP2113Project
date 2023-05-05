c5a901d141f13da9bd947b6954ed3b6baaca377e
Tsasng-Tsz-Ho <127906711+Tsasng-Tsz-Ho@users.noreply.github.com>
2023-05-05 17:40:21 +0800

    Comment Search.cpp

--- Search.cpp
+++ Search.cpp
@@ -5,6 +5,10 @@
 using namespace std;
 
 int Search(vector<Player> user_team,string name){
+  //It search for the player by name
+  //Arg 1: the team to be searched
+  //Arg 2: the target name
+  //return value: the position of the target player in the team
   for (int i=0;i<user_team.size();i++){
     if (user_team[i].name==name){
       return i;

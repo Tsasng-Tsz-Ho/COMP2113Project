b451d9b5a363069952955b8fb1a7ca27d5e74c59
Ernestyltsang <125571142+Ernestyltsang@users.noreply.github.com>
2023-05-05 20:54:58 +0800

    Update GamePlay.cpp

--- GamePlay.cpp
+++ GamePlay.cpp
@@ -9,7 +9,6 @@ string GamePlay(MatchStatus &match){
   //Arg: a struct of match status that contains both teams, turn and scores passed by reference
   //return value: if the player choose to save during a match, it returns a string "save" and saving is done in main
   //else, the match is finished normally and return "finish"
-  int ran_num;
   string ran_direction;
   string direction;
   //The for loop iterates all offensive players have a chance to shoot
@@ -22,8 +21,7 @@ string GamePlay(MatchStatus &match){
       cout<<"Enter L if you want to shoot to the left, M to shoot to the middle, R to shoot to the right, S to save."<<endl;
       cin>>direction;
       if(direction=="S"){return "save";}
-      ran_num = rand()%3;
-      ran_direction = CompDirection(ran_num);
+      ran_direction = CompDirection();
       int result = Score(shooter,gk,direction,ran_direction);
       match.user_score += result;
     } else{
@@ -35,7 +33,7 @@ string GamePlay(MatchStatus &match){
       cin>>direction;
       if(direction=="S"){return "save";}
       ran_num = rand()%3;
-      ran_direction = CompDirection(ran_num);
+      ran_direction = CompDirection();
       int result = Score(shooter,gk,ran_direction,direction);
       match.opponent_score += result;
     }
